6289356451fabfa2e03d0349a85ac193ad14eb1c
7e_nv <pycartem@mail.ru>
2025-07-28 03:22:12 +1000

    Upd

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ const tg = window.Telegram.WebApp;
 
 function App() {
   const [thumbnail, setThumbnail] = useState('');
+  const [username, setUsername] = useState('');
 
   useEffect(() => {
     tg.ready()
   }, [])
 
   useEffect(async () => {
-    const response = await axios.get(`https://api.telegram.org/api/thumbnail/id/${tg.initDataUnsafe.user.id}`);
+    const response = await axios.get(`http://45.131.40.90/api/thumbnail/id/${tg.initDataUnsafe.user.id}`);
     setThumbnail(response.data);
   }, [])
 
@@ -20,13 +21,20 @@ function App() {
     tg.close();
   }
 
+  const saveUser = async () => {
+    await axios.post(`http://45.131.40.90/api/thumbnail`, JSON.stringify({initData: tg.initData, data: {username: username}}))
+  }
+
+
   return (
-    <div className="App">
-      <span>{thumbnail.imageUrl}</span>
-      <span>{thumbnail.username}</span>
-      <button onClick={onClose}>Закрыть</button>
-    </div>
-  );
+  <div className="App">
+    <span>{thumbnail.imageUrl}</span>
+    <span>{thumbnail.username}</span>
+    <input onChange={(event) => {setUsername(event.target.value)}}>{username}</input>
+    <button onClick={saveUser}>Сохранить ник</button>
+    <button onClick={onClose}>Закрыть</button>
+  </div>
+)
 }
 
 export default App;
