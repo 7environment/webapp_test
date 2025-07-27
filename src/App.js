fb6109edbc5778d00f642a0b411ad97a29ab3c86
7e_nv <pycartem@mail.ru>
2025-07-28 03:25:05 +1000

    Upd

--- src/App.js
+++ src/App.js
@@ -5,16 +5,10 @@ import axios from "axios";
 const tg = window.Telegram.WebApp;
 
 function App() {
-  const [thumbnail, setThumbnail] = useState('');
   const [username, setUsername] = useState('');
 
   useEffect(() => {
     tg.ready()
-  }, [])
-
-  useEffect(async () => {
-    const response = await axios.get(`http://45.131.40.90/api/thumbnail/id/${tg.initDataUnsafe.user.id}`);
-    setThumbnail(response.data);
   }, [])
 
   const onClose = () => {
@@ -28,8 +22,6 @@ function App() {
 
   return (
   <div className="App">
-    <span>{thumbnail.imageUrl}</span>
-    <span>{thumbnail.username}</span>
     <input onChange={(event) => {setUsername(event.target.value)}}>{username}</input>
     <button onClick={saveUser}>Сохранить ник</button>
     <button onClick={onClose}>Закрыть</button>
