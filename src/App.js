1df85a4f302901de9f78f869fc8e7423fdef4576
7e_nv <pycartem@mail.ru>
2025-07-28 04:00:51 +1000

    maybe work?

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import {useEffect, useState} from "react";
-import axios from "axios";
 const tg = window.Telegram.WebApp
 
 function App() {
-  const [username, setUsername] = useState("");
   useEffect(() => {
     tg.ready()
   }, []);
-
-  async function saveUser() {
-    const responce = await axios.post("http://45.131.40.90/api/thumbnail", {initData: tg.initData, data: {username: username}});
-    console.log(responce);
-  }
 
   const onClose = () => {
     tg.close();
@@ -21,8 +13,8 @@ function App() {
   return (
     <div className="App">
       <button onClick={onClose}>Exit</button>
-      <input onChange={(e) => {setUsername(e.target.value)}} type="text">{username}</input>
-      <button onClick={saveUser}>Save</button>
+      <span>{tg.initData}</span>
+      <span>{tg.initDataUnsafe.user.id}</span>
     </div>
   );
 }
