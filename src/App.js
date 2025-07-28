088d116821ec5ba9907d47ab64d4ef23dea31256
7e_nv <pycartem@mail.ru>
2025-07-29 01:46:54 +1000

    is it work?

--- src/App.js
+++ src/App.js
@@ -4,29 +4,77 @@ import axios from "axios";
 const tg = window.Telegram.WebApp
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [debug, setDebug] = useState("");
+  const [userProfile, setUserProfile] = useState({username: "", thumbnail: null});
+  const [isChoosing, setIsChoosing] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function loadThumbnail() {
+    const response = await axios.get(`https://swixgodly.ru/api/thumbnail/id/${tg.initDataUnsafe.user.id}`)
+    if (response.status === 200) {
+      setUserProfile({username: response.data.username, thumbnail: response.data.imageUrl});
+    } else {
+      setIsChoosing(true);
+    }
+  }
+
+  async function saveUser() {
+    const response = await axios.post(`https://swixgodly.ru/api/username`, {initData: tg.initData, data: {username: userProfile.username}});
+    if (response.status === 200) {
+      setIsChoosing(false);
+    }
+  }
+
+  async function getThumbnail(username) {
+      if (username.length > 3) {
+          setIsLoading(true);
+          const response = await axios.get(`https://swixgodly.ru/api/thumbnail/${username}`);
+          setIsLoading(false);
+          if (response.status === 200) {
+              setUserProfile({username: username, thumbnail: response.data.imageUrl})
+          } else {
+              setUserProfile({username: username, thumbnail: null});
+          }
+      } else {
+          setUserProfile({username: username, thumbnail: null});
+      }
+  }
 
   useEffect(() => {
     tg.ready()
+    loadThumbnail()
   }, []);
 
   const onClose = () => {
     tg.close();
   }
 
-  async function saveUser() {
-    const response = await axios.post("https://swixgodly.ru/api/username", {initData: tg.initData, data: {username: username}})
-    setDebug(response.data)
+  const chooseAnother = () => {
+      setUserProfile(null)
+      setIsChoosing(true);
   }
 
   return (
+    isChoosing ?
     <div className="App">
-      <h1>version 1</h1>
-      <button onClick={onClose}>Exit</button>
-      <input onChange={(e) => {setUsername(e.target.value)}} type="text" value={username} />
-      <button onClick={saveUser}>Сохранить</button>
-      <h1>{debug.username}</h1>
+      <button onClick={onClose}>Выйти из приложения</button>
+        {isLoading ?
+          <h3>Загрузка...</h3>
+        :
+          userProfile.thumbnail ?
+            <img src={userProfile.thumbnail} alt="profile"/>
+          :
+            <h3>Аккаунта с таким ником нет</h3>
+        }
+      <input value={userProfile.username} onChange={(e) => {getThumbnail(e.target.value)}} type="text"/>
+      <button disabled={userProfile.thumbnail === null} onClick={saveUser}>Выбрать этот аккаунт</button>
+    </div>
+        :
+    <div className="App">
+      <button onClick={onClose}>Выйти из приложения</button>
+      <img src={userProfile.thumbnail} alt="profile"/>
+      <h3>{userProfile.username}</h3>
+      <button>Выбрать этот аккаунт</button>
+      <button onClick={chooseAnother}>Выбрать другой аккаунт</button>
     </div>
   );
 }
